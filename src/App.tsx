c452c8dfee38d2058521c1c4075b7334b013af84
Thalys001 <thalyssouza708@gmail.com>
2023-02-09 01:02:58 -0300

    style(search/desktop): component styling search

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import {Outlet} from 'react-router-dom'
 
-function App() {
+import classes from './app.module.css'
 
+function App() {
   return (
-    <div className="App">
+    <div className={classes.app}>
       <h1>Github Finder</h1>
       <Outlet/>
     </div>
