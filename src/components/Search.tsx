701e25d9549693dacb6bf74267b3638ad926304f
Thalys001 <thalyssouza708@gmail.com>
2023-02-09 01:06:45 -0300

    feat(search): added onKeyDown function

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -11,13 +11,20 @@ import classes from './Search.module.css'
 const Search = ({loadUser}: SearchProps) => {
     const [userName, setUserName] = useState('')
 
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if (e.key === 'Enter') {
+            loadUser(userName)
+        }
+    }
+
     return (
         <div className={classes.search}>
             <h2>Busque por um usuário</h2>
             <p>Conheça seus melhores repositórios</p>
             <div className={classes.search_container} >
                 <input type='text' placeholder='Digite o nome do usuário' 
-                onChange={(e) => setUserName(e.target.value)} 
+                onChange={(e) => setUserName(e.target.value)}
+                onKeyDown={handleKeyDown}
                 />
                 <button onClick={() => loadUser(userName)} >
                     <BsSearch/>
